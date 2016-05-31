49d1a98e8a3f5ad648cf6268067b7c90951d9eae
Paul Crestez <pmc15@corona12.doc.ic.ac.uk>
2016-05-31 15:32:35 +0100

    Implemented game of life better

--- game_of_life.s
+++ game_of_life.s
@@ -34,30 +34,69 @@ ldr r7,=0x0ffd                 @ &a
 @ 5
 @
 gol_main:
+
+  stmfd sp!,{r0-r14}             @ call graphic_initialise
+  mov r0,r5
+  mov r1,r6
+  bl graphics_initialise
+  ldmfd sp!,{r0-r14}
+
+  ldr r5,=32                     @ d1
+  ldr r6,=32                     @ d2
+  ldr r7,=0x0ffd                 @ &a
   mov r1,#0                      @ i = 0
   mov r2,#0                      @ j = 0
-  call gol_set_alive
+  stmfd sp!,{r14}
+  bl gol_set_alive
+  ldmfd sp!,{r14}
   mov r1,#1                      @ i = 1
   mov r2,#0                      @ j = 0
-  call gol_set_alive
+  stmfd sp!,{r14}
+  bl gol_set_alive
+  ldmfd sp!,{r14}
   mov r1,#0                      @ i = 0
   mov r2,#1                      @ j = 1
-  call gol_set_alive
+  stmfd sp!,{r14}
+  bl gol_set_alive
+  ldmfd sp!,{r14}
   mov r1,#1                      @ i = 1
   mov r2,#1                      @ j = 1
-  call gol_set_alive
+  stmfd sp!,{r14}
+  bl gol_set_alive
+  ldmfd sp!,{r14}
   mov r1,#4                      @ i = 4
   mov r2,#1                      @ j = 1
-  call gol_set_alive
+  stmfd sp!,{r14}
+  bl gol_set_alive
+  ldmfd sp!,{r14}
   mov r1,#4                      @ i = 4
   mov r2,#2                      @ j = 2
-  call gol_set_alive
+  stmfd sp!,{r14}
+  bl gol_set_alive
+  ldmfd sp!,{r14}
   mov r1,#4                      @ i = 4
   mov r2,#3                      @ j = 3
-  call gol_set_alive
+  stmfd sp!,{r14}
+  bl gol_set_alive
+  ldmfd sp!,{r14}
   gol_skip_import:
-  call gol_game_tick
-  call gol_game_tick
+
+  stmfd sp!,{r14}
+  bl gol_game_tick
+  ldmfd sp!,{r14}
+
+  mov r1,#0x1000000
+  gol_countdown_loop
+    cmp r1,#0
+    be gol_countdown_end
+    sub r1,r1,#1
+    b gol_countdown_loop
+  gol_countdown_end:
+
+  stmfd sp!,{r14}
+  bl gol_game_tick
+  ldmfd sp!,{r14}
+
   andeq r0,r0,r0  @ for emulator use
   gol_break:
     b gol_break
@@ -72,6 +111,9 @@ gol_main:
 @ effect
 @   advances the game of life by 1 generation
 gol_game_tick:
+  ldr r5,=32                     @ d1
+  ldr r6,=32                     @ d2
+  ldr r7,=0x0ffd                 @ &a
   mov r1,#0                      @ i = 0
   gol_loop1:                     @ for(i = 0 -> d1)
     cmp r1,r5
@@ -81,38 +123,61 @@ gol_game_tick:
       cmp r2,r6
       beq gol_loop1_1_end
     @ loop1_1 body
-      call gol_get_alive
+      stmfd sp!,{r14}
+      bl gol_get_alive
+      ldmfd sp!,{r14}
       cmp r3,#0                    @ if cell is dead, no need to update neighbours
       beq gol_loop1_1_if_end
-      push r2
-      push r1
-      push r2
-      push r1
-      push r2
+      stmfd sp!,{r2}
+      stmfd sp!,{r1}
+      stmfd sp!,{r2}
+      stmfd sp!,{r1}
+      stmfd sp!,{r2}
       sub r1,r1,#1                 @ NW
       sub r2,r2,#1
-      call gol_cycle
-      call gol_get_status
-      add r3,r3,#0x01000000           @ add 1 alive neighbour
-      call gol_put
-      pop r2                       @ N
-      call gol_add_neighbour
+      stmfd sp!,{r14}
+      bl gol_cycle
+      ldmfd sp!,{r14}
+      stmfd sp!,{r14}
+      bl gol_add_neighbour
+      ldmfd sp!,{r14}
+      ldmfd sp!,{r2}               @ N
+      stmfd sp!,{r14}
+      bl gol_add_neighbour
+      ldmfd sp!,{r14}
       add r2,r2,#1                 @ NE
-      call gol_cycle
-      call gol_add_neighbour
-      pop r1                       @ E
-      call gol_add_neighbour
+      stmfd sp!,{r14}
+      bl gol_cycle
+      stmfd sp!,{r14}
+      bl gol_add_neighbour
+      ldmfd sp!,{r14}
+      ldmfd sp!,{r1}               @ E
+      stmfd sp!,{r14}
+      bl gol_add_neighbour
+      ldmfd sp!,{r14}
       add r1,r1,#1                 @ SE
-      call gol_cycle
-      call gol_add_neighbour
-      pop r2                       @ S
-      call gol_add_neighbour
+      stmfd sp!,{r14}
+      bl gol_cycle
+      ldmfd sp!,{r14}
+      stmfd sp!,{r14}
+      bl gol_add_neighbour
+      ldmfd sp!,{r14}
+      ldmfd sp!,{r2}               @ S
+      stmfd sp!,{r14}
+      bl gol_add_neighbour
+      ldmfd sp!,{r14}
       sub r2,r2,#1                 @ SW
-      call gol_cycle
-      call gol_add_neighbour
-      pop r1                       @ W
-      call gol_add_neighbour
-      pop r2
+      stmfd sp!,{r14}
+      bl gol_cycle
+      ldmfd sp!,{r14}
+      stmfd sp!,{r14}
+      bl gol_add_neighbour
+      ldmfd sp!,{r14}
+      ldmfd sp!,{r1}               @ W
+      stmfd sp!,{r14}
+      bl gol_add_neighbour
+      ldmfd sp!,{r14}
+      ldmfd sp!,{r2}
       gol_loop1_1_if_end:
     @ end loop1_1 body
       add r2,r2,#1
@@ -121,6 +186,7 @@ gol_game_tick:
     add r1,r1,#1
     b gol_loop1
   gol_loop1_end:
+
   mov r1,#0                      @ i = 0
   gol_loop2:                     @ for(i = 0 -> d1)
     cmp r1,r5
@@ -130,23 +196,47 @@ gol_game_tick:
       cmp r2,r6
       beq gol_loop2_1_end
     @ loop2_1 body  
-      call gol_get_neighbours
+      stmfd sp!,{r14}
+      bl gol_get_neighbours
+      ldmfd sp!,{r14}
       cmp r3,#3
       bne gol_update_endif_1
       b gol_alive
       gol_update_endif_1:
       cmp r3,#2
       bne gol_dead
-      call gol_get_alive
+      stmfd sp!,{r14}
+      bl gol_get_alive
+      ldmfd sp!,{r14}
       cmp r3,#1
       bne gol_dead
       gol_alive:
-        call gol_set_alive
-        @ TODO call gol_martin_graphic
+        stmfd sp!,{r14}
+        bl gol_set_alive
+        ldmfd sp!,{r14}
+        
+        stmfd sp!,{r0-r14}             @ call graphic_alive
+        mov r0,r1
+        mov r1,r2
+        mov r2,#0xffff
+        bl graphics_draw_pixel
+        ldmfd sp!,{r0-r14}
+
+        bl gol_set_alive
+        ldmfd sp!,{r14}
         b gol_update_end
       gol_dead:
-        call gol_set_dead
-        @ TODO call gol_martin_graphic
+        stmfd sp!,{r14}
+        bl gol_set_dead
+        ldmfd sp!,{r14}
+        
+        stmfd sp!,{r0-r14}             @ call graphic_dead
+        mov r0,r1
+        mov r1,r2
+        mov r2,#0
+        bl graphics_draw_pixel
+        ldmfd sp!,{r0-r14}
+
       gol_update_end:
     @ end loop2_1 body
       add r2,r2,#1
@@ -155,7 +245,7 @@ gol_game_tick:
     add r1,r1,#1
     b gol_loop2
   gol_loop2_end:
-  return
+  mov pc,lr                        @ return
 
 @ get_alive
 @ input
@@ -166,10 +256,12 @@ gol_game_tick:
 @ output
 @   r3 - if a[i][j] lives
 gol_get_alive:
-  call gol_get_status
+  stmfd sp!,{r14}
+  bl gol_get_status
+  ldmfd sp!,{r14}
   and r3,r3,#0x80000000            @ select the alive status, which is bit 0
   mov r3,r3,lsr,#31
-  return
+  mov pc,lr                        @ return
 
 @ get_neighbours
 @ input
@@ -180,10 +272,12 @@ gol_get_alive:
 @ output
 @   r3 - how many alive neighbour a[i][j] has
 gol_get_neighbours:
-  call gol_get_status
+  stmfd sp!,{r14}
+  bl gol_get_status
+  ldmfd sp!,{r14}
   and r3,r3,#0x0f000000            @ select the neighbour sum, which is bit 4-7
   mov r3,r3,lsr,#24
-  return
+  mov pc,lr                        @ return
 
 @ get_status
 @   r1 - i coordonate
@@ -198,7 +292,7 @@ gol_get_status:
   add r3,r3,r2
   add r3,r3,r7
   ldr r3,[r3]
-  return
+  mov pc,lr                        @ return
 
 @ add_neighbour
 @ input
@@ -212,10 +306,14 @@ gol_get_status:
 @ effect
 @   neighbour[i][j]++
 gol_add_neighbour:
-  call gol_get_status
-  add r3,r3,#0x01000000           @ add 1 alive neighbour
-  call gol_put
-  return
+  stmfd sp!,{r14}
+  bl gol_get_status
+  ldmfd sp!,{r14}
+  add r3,r3,#0x01000000            @ add 1 alive neighbour
+  stmfd sp!,{r14}
+  bl gol_put
+  ldmfd sp!,{r14}
+  mov pc,lr                        @ return
 
 @ cycle
 @ input
@@ -242,7 +340,7 @@ gol_cycle:
   blt gol_cycle_endif_4
   sub r2,r2,r6
   gol_cycle_endif_4:
-  return
+  mov pc,lr                        @ return
 
 @ put
 @   r1 - i coordonate
@@ -258,7 +356,7 @@ gol_put:
   add r4,r4,r2
   add r4,r4,r7
   str r3,[r4]
-  return
+  mov pc,lr                        @ return
 
 @ set_alive
 @   r1 - i coordonate
@@ -271,12 +369,16 @@ gol_put:
 @ effect
 @   a[i][j] becomes alive
 gol_set_alive:
-  call gol_get_status
+  stmfd sp!,{r14}
+  bl gol_get_status
+  ldmfd sp!,{r14}
   ldr r4,=0x00ffffff
-  and r3,r3,r4                   @ restart the cell
-  add r3,r3,#0x80000000          @ make the cell alive
-  call gol_put
-  return
+  and r3,r3,r4                     @ restart the cell
+  add r3,r3,#0x80000000            @ make the cell alive
+  stmfd sp!,{r14}
+  bl gol_put
+  ldmfd sp!,{r14}
+  mov pc,lr                        @ return
 
 @ set_dead
 @   r1 - i coordonate
@@ -289,8 +391,12 @@ gol_set_alive:
 @ effect
 @   a[i][j] becomes dead
 gol_set_dead:
-  call gol_get_status
+  stmfd sp!,{r14}
+  bl gol_get_status
+  ldmfd sp!,{r14}
   ldr r4,=0x00ffffff
-  and r3,r3,r4                   @ restart the cell
-  call gol_put
-  return
+  and r3,r3,r4                     @ restart the cell
+  stmfd sp!,{r14}
+  bl gol_put
+  ldmfd sp!,{r14}
+  mov pc,lr                        @ return
