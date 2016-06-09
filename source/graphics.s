da17395aa5703b6595624f5df78fe70450916fa6
Martin Zlocha <zlocha.martin@gmail.com>
2016-06-09 19:58:33 +0100

    Improved style in the graphical part of the code.

--- source/graphics.s
+++ source/graphics.s
@@ -1,21 +1,33 @@
-.global set_graphics_address
-.global graphics_draw_pixel
-.global graphics_draw_square
+.global     set_graphics_address
+.global     graphics_draw_pixel
+.global     graphics_draw_square
 
+@-------------------------------------------------------------------------------
+@ set_graphics_address*
+@ Effect:
+@   Saves the address of the frame buffer so that we can draw to it.
+@ Arguments:
+@   r0 - graphical frame buffer address
+@ Returns:
+@   none
+@ Clobbers:
+@   r0-r1
+@-------------------------------------------------------------------------------
+.section .text
+set_graphics_address:
+  ldr       r1, =graphics_address
+  str       r0, [r1]
+  mov       pc, lr
+    
 .section .data
 .align 2
 graphics_address:
-    .int 0
-
-.section .text
-set_graphics_address:
-    ldr       r1, =graphics_address
-    str       r0, [r1]
-    mov       pc, lr
-
+  .int 0
 
 @-------------------------------------------------------------------------------
-@ Renders a pixel of colour r2 at (r0, r1)
+@ graphics_draw_pixel*
+@ Effect:
+@   Renders a pixel of colour r2 at (r0, r1)
 @ Arguments:
 @   r0 - x coordinate
 @   r1 - y coordinate
@@ -26,36 +38,42 @@ set_graphics_address:
 @   none
 @-------------------------------------------------------------------------------
 graphics_draw_pixel:
-    stmfd     sp!, {r0 - r4, lr}
+  stmfd     sp!, {r0 - r4, lr}
 
-    x .req r0
-    y .req r1
-    color .req r2
-	address .req r3
+  x         .req r0
+  y         .req r1
+  color     .req r2
+  address   .req r3
 	
-	ldr address,=graphics_address
-	ldr address,[address]
+  @ Get address of the frame buffer structure
+  ldr       address, =graphics_address
+  ldr       address, [address]
 	
-	width .req r4
-	ldr width,[address, #0]
+  width     .req r4
+  ldr       width, [address, #0]
 	
-	fb_address .req r3
-	ldr fb_address,[address, #32]
-	.unreq address
+  @ Gets the address to the actual frame buffer
+  fb_addr   .req r3
+  ldr       fb_addr, [address, #32]
+  .unreq    address
 	
-	mla x, y, width, x
-	add fb_address, x,lsl #1
-	.unreq y
-	.unreq x
+  @ fb_addr = fb_addr + y * width + x
+  mla       x, y, width, x
+  add       fb_addr, x, lsl #1
+  .unreq    y
+  .unreq    x
 	
-	strh color, [fb_address]
-	.unreq color
-	.unreq fb_address
+  @ Store the color
+  strh      color, [fb_addr]
+  .unreq    color
+  .unreq    fb_addr
     
-    ldmfd     sp!, {r0 - r4, pc} 
+  ldmfd     sp!, {r0 - r4, pc} 
     
 @-------------------------------------------------------------------------------
-@ Renders a a suare of colour r2 at (r0 * square_size, r1 * square_size)
+@ graphics_draw_square*
+@ Effect:
+@   Renders a a suare of colour r2 at (r0 * square_size, r1 * square_size)
 @ Arguments:
 @   r0 - x
 @   r1 - y
@@ -66,48 +84,54 @@ graphics_draw_pixel:
 @   none
 @-------------------------------------------------------------------------------
 graphics_draw_square:
-	stmfd     sp!, {r0 - r8, lr}
+  stmfd     sp!, {r0 - r8, lr}
 
-    curr_x .req r0
-    curr_y .req r1
-    x .req r3
-    y .req r4
-    max_x .req r5
-    max_y .req r6
+  curr_x    .req r0
+  curr_y    .req r1
+  x         .req r3
+  y         .req r4
+  max_x     .req r5
+  max_y     .req r6
+  
+  @ Move arguments into other registers so that we can use r0 and r1
+  mov       x, r0
+  mov       y, r1
     
-    mov x, r0
-    mov y, r1
+  @ Gets the spacing between individual grid squares
+  grid_size .req r7
+  ldr       grid_size, =fb_grid_size
+  ldr       grid_size, [grid_size]
+  
+  @ Gets the size of the square within the grid. Different from grid_size so
+  @ that we have a border
+  sq_size   .req r8
+  ldr       sq_size, =fb_square_size
+  ldr       sq_size, [sq_size]
     
-    grid_size .req r7
-    ldr grid_size,=fb_grid_size
-    ldr grid_size,[grid_size]
+  @ Transforms the grid coordinates into the actual coordinates on the screen
+  mul       curr_x, x, grid_size
+  mul       curr_y, y, grid_size
+  
+  @ Calculates where the square should end
+  mov       max_x, curr_x
+  add       max_x, sq_size
     
-    square_size .req r8
-    ldr square_size,=fb_square_size
-    ldr square_size,[square_size]
+  mov       max_y, curr_y
+  add       max_y, sq_size
     
-    mul curr_x, x, grid_size
-    mul curr_y, y, grid_size
-    
-    mov max_x, curr_x
-    add max_x, square_size
-    
-    mov max_y, curr_y
-    add max_y, square_size
-    
-    drawRow:
-		drawPixel:
-			bl graphics_draw_pixel
+  drawRow:
+    drawPixel:
+      bl        graphics_draw_pixel
 			
-			add curr_x,#1
-			cmp curr_x, max_x
-			ble drawPixel
+      add       curr_x,#1
+      cmp       curr_x, max_x
+			ble       drawPixel
 
-		mul curr_x, x, grid_size
+		mul       curr_x, x, grid_size
 			
-		add curr_y,#1
-		cmp curr_y, max_y
-		ble drawRow
+  add       curr_y,#1
+  cmp       curr_y, max_y
+  ble       drawRow
     
-    ldmfd     sp!, {r0 - r8, pc} 
+  ldmfd     sp!, {r0 - r8, pc} 
 
--- source/mailbox.s
+++ source/mailbox.s
@@ -1,120 +1,128 @@
-.global get_mailbox_base
-.global mailbox_write
-.global mailbox_read
+.global      get_mailbox_base
+.global      mailbox_write
+.global      mailbox_read
 
+@-------------------------------------------------------------------------------
+@ get_mailbox_base*
+@ Effect:
+@   Returns the base pointer to the mailbox
+@ Arguments: 
+@   none
+@ Returns:
+@   r0 - pointer to the mailbox
+@ Clobbers:
+@   none
+@-------------------------------------------------------------------------------
 get_mailbox_base:
-    ldr     r0, =0x2000B880
-    mov     pc, lr
+  ldr       r0, =0x2000B880
+  mov       pc, lr
 
-@------------------------------------------------------------------------------
-@ Mailbox Write
+@-------------------------------------------------------------------------------
+@ mailbox_write*
 @ Effect:
 @   Writes data to the mailbox supplied as an argument
 @ Arguments: 
-@   r0 = what to write
-@   r1 = what mailbox to write it to
+@   r0 - what to write
+@   r1 - what mailbox to write it to
 @ Returns:
-@   r0 = Result of write   
+@   r0 - Result of write   
 @ Clobbers:
-@   r0, r1
-@------------------------------------------------------------------------------
+@   none
+@-------------------------------------------------------------------------------
 mailbox_write:
-    @ Test lowest 4 bits of r0 are 0
-    tst     r0, #0b1111
-    movne   pc, lr
+  @ Test lowest 4 bits of r0 are 0
+  tst       r0, #0b1111
+  movne     pc, lr
 
-    @ Check that r1 is an existing mailbox
-    cmp     r1, #15
-    movhi   pc, lr
+  @ Check that r1 is an existing mailbox
+  cmp       r1, #15
+  movhi     pc, lr
 
-    @ Store registers and return
-    stmfd   sp!, {r2-r10, lr}
+  @ Store registers and return
+  stmfd     sp!, {r1-r3, lr}
 
-    @ Set register names and set r0 = mailbox base
-    channel .req  r1
-    value   .req  r2
-    mov     value, r0
-    bl      get_mailbox_base
-    mailbox .req  r0
+  @ Set register names and set r0 = mailbox base
+  channel   .req  r1
+  value     .req  r2
+  mov       value, r0
+  bl        get_mailbox_base
+  mailbox   .req  r0
 
-    @ Wait until the status of the mailbox is correct
-    1:
-        status    .req r3
-        ldr       status, [mailbox, #0x18]
-      
-        tst       status, #0x80000000
-        .unreq    status
-        bne       1b
+  @ Wait until the status of the mailbox is correct
+  1:
+    status    .req r3
+    ldr       status, [mailbox, #0x18]
+  
+    tst       status, #0x80000000
+    .unreq    status
+    bne       1b
 
-    @ Add the channel to the value
-    add     value, channel
-    .unreq  channel
+  @ Add the channel to the value
+  add       value, channel
+  .unreq    channel
 
-    @ Store the result to the write field
-    str     value, [mailbox, #0x20]
-    .unreq  value
-    .unreq  mailbox
+  @ Store the result to the write field
+  str       value, [mailbox, #0x20]
+  .unreq    value
+  .unreq    mailbox
 
-    @ Return
-    ldmfd   sp!, {r2-r10, pc}
+  @ Return
+  ldmfd     sp!, {r1-r3, pc}
 
 
 @------------------------------------------------------------------------------
-@ Mailbox Read
+@ mailbox_read*
 @ Effect:
 @   Reads the value in the mailbox of the supplied argument
 @ Arguments: 
-@   r0 = which mailbox to read from
+@   r0 - which mailbox to read from
 @ Returns:
-@   r0 = what was in the mailbox
+@   r0 - what was in the mailbox
 @ Clobbers:
-@   r0
+@   none
 @------------------------------------------------------------------------------
 mailbox_read:
-    @ Test that r0 is an existing mailbox
-    cmp     r0, #15
-    movhi   pc, lr
+  @ Test that r0 is an existing mailbox
+  cmp       r0, #15
+  movhi     pc, lr
 
+  @ Store registers and return
+  stmfd     sp!, {r1-r10, lr}
 
-    @ Store registers and return
-    stmfd   sp!, {r1-r10, lr}
+  @ Set names and get mailbox base
+  channel   .req  r1
+  mov       channel, r0
+  bl        get_mailbox_base
+  mailbox   .req  r0
 
-    @ Set names and get mailbox base
-    channel .req  r1
-    mov     channel, r0
-    bl      get_mailbox_base
-    mailbox .req  r0
+  1:
+    2:
+      @ Gets the current status
+      status    .req r2
+      ldr       status, [mailbox, #0x18]
+      
+      @ Checks that the 30th bit of status is 0 or waits
+      tst       status, #0x40000000
+      .unreq    status
+      bne       2b
+    
+    @ Read next mailbox item
+    mail      .req r2
+    ldr       mail, [mailbox, #0] 
 
-    1:
-        2:
-            @ Gets the current status
-            status  .req r2
-            ldr     status, [mailbox, #0x18]
-            
-            @ Checks that the 30th bit of status is 0 or waits
-            tst     status, #0x40000000
-            .unreq  status
-            bne     2b
-        
-        @ Read next mailbox item
-        mail    .req r2
-        ldr     mail, [mailbox, #0] 
+    @ Check that the mail channel is right or goes back
+    inchannel .req r3
+    and       inchannel, mail, #0b1111
+    teq       inchannel, channel
+    .unreq    inchannel
+    bne       1b
 
-        @ Check that the mail channel is right or goes back
-        inchannel .req r3
-        and     inchannel, mail, #0b1111
-        teq     inchannel, channel
-        .unreq  inchannel
-        bne     1b
+  .unreq    mailbox
+  .unreq    channel
 
+  @ Move the answer to r0
+  and r0,   mail, #0xfffffff0
+  .unreq    mail
 
-    .unreq  mailbox
-    .unreq  channel
-
-    @ Move the answer to r0
-    and r0, mail, #0xfffffff0
-    .unreq  mail
-
-    @ Return
-    ldmfd sp!, {r1-r10, pc}
-
+  @ Return
+  ldmfd     sp!, {r1-r10, pc}
