1fe9bfb4d57ffc44dc8807bc311aba57e7cae8f8
iarba <dociarba@gmail.com>
2016-06-30 13:53:53 +0100

    Cleaned up main, reordered some operations
    Checking for input is now the first thing done.
    Playing any sound now restarts the edge of the interrupt signal
      As such, interrupt can toggle after only 1 generation.
      The button can be held at the end of the first generation.

--- source/main.s
+++ source/main.s
@@ -11,15 +11,24 @@ main:
     bl      setup_sound
     bl      read_init
 
-	color .req r2
-	ldr color, =0xffff    
+    color .req r2
+    ldr color, =0xffff    
 
     @bl      test_get_sound
     @bl      sound_test_fill
     bl      gol_main
-    mov     r4,#0                  @ signal 04 starts as 0
+    mov     r4, #0                 @ signal 17 starts as 0
 
 render:
+    stmfd   sp!,{r3}
+    bl      read_check
+    cmp     r7, #0
+    beq     main_skip_interrupt
+    bl      read_main
+    mov     r4, #0x00020000        @ signal 17 was up
+main_skip_interrupt:
+    ldmfd   sp!, {r3}
+
     bl      sound_of_life
 
     @ Pause between ticks if playing sounds
@@ -27,16 +36,8 @@ render:
     ldr     r1, [r1]
     cmp     r1, #0 
     movne   r0, #0
+    movne   r4, #0                 @ signal 17 restarts as 0
     blne    play_sound
-
-    stmfd   sp!,{r3}
-    bl      read_check
-    cmp     r7,#0
-    beq     main_skip_interrupt
-    bl      read_main
-    mov     r4,#0x00020000         @ signal 04 was up
-main_skip_interrupt:
-    ldmfd   sp!,{r3}
 
     bl      gol_game_tick
 
